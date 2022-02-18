1b9fd3f0d9931853ea1c7a96e67753b87401b49b
safarlisadiq <95844351+safarlisadiq@users.noreply.github.com>
2022-02-18 10:57:16 +0400

    Add files via upload

---
+++ 1.6/script.js
@@ -0,0 +1,21 @@
+
+let java = "JAva";
+
+let test = document.getElementById('trjs');
+
+test.innerHTML = java;
+
+let num = 5;
+num=6;
+console.log(num);
+let name = 'Sadiq';
+    let age = 24;
+    let surname ='Safarli';
+    let military = true;
+    test.innerHTML=`Ad:${name} <br> Yas:${age} <br> Soyad:${surname} <br> Herbi vezife:${military}`;
+
+
+    alert('Salam,Sehifemize xos gelmissiniz');
+   let numune= prompt('istifadeci adinizi daxil edin ','istifadeci adinda bir boyuk herf ve en azi 1 reqem daxil ediniz');
+   test.innerHTML=numune;
+   confirm('Sehifemizi terk etmek isteyidinize eminsinizmi?')
